0f57b8f2a8244badece796268523b0fa183f6ffc
marco julio aparicio <mjaf83@gmail.com>
2025-04-20 21:10:22 +0100

    Chat with your FAISS PDs

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import os
 from dotenv import load_dotenv
+from langchain.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.vectorstores import FAISS
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain import hub
 
 
 
@@ -7,6 +14,31 @@ load_dotenv()
 
 if __name__ == "__main__":
     print("Hello React LangChain!")
-    pdf_path = 'C:\Users\cario\Documents\langchain_v2\vectorstor-in-memory\2210.03629v3.pdf'
+    pdf_path = r"C:\Users\cario\Documents\langchain_v2\vectorstor-in-memory\2210.03629v3.pdf"
+    loader = PyPDFLoader(file_path=pdf_path)
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
+    docs = text_splitter.split_documents(documents=documents)
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(docs, embeddings)
+    vectorstore.save_local("faiss_index_react")
+    print("Ingestion complete.") 
+    new_vectorstore = FAISS.load_local("faiss_index_react", embeddings, allow_dangerous_deserialization=True) 
+    print("Loading complete.")
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    llm = ChatOpenAI(temperature=0, model="gpt-4.1-nano")
+    combine_docs_chain = create_stuff_documents_chain(llm=llm, prompt=retrieval_qa_chat_prompt)
+    retrieval_chain = create_retrieval_chain(
+        retriever=new_vectorstore.as_retriever(),
+        combine_docs_chain=combine_docs_chain
+    )
+    query = "Give me the gist of reaact in 3 sentences in spanish."
+    #query = "Dime un resumen de todo el documento en tres lineas en español."
+    result = retrieval_chain.invoke({"input": query})
+    print(result.get("answer"))
+
+        
+
+
 
 
