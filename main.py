6e4cfd7ec107d69c0fc9243275a0788aa8b7c749
marco julio aparicio <mjaf83@gmail.com>
2025-04-20 20:20:38 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+if __name__ == "__main__":
+    print("Hello React LangChain!")
+    pdf_path = 'C:\Users\cario\Documents\langchain_v2\vectorstor-in-memory\2210.03629v3.pdf'
+
+
